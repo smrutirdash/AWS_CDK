c224145d532ac60d077be54c1e4f5ba6b327eb7f
smrutirdash <78108561+smrutirdash@users.noreply.github.com>
2025-03-16 00:33:02 -0500

    account and region setting

--- bin/aws_cdk.ts
+++ bin/aws_cdk.ts
@@ -7,3 +7,8 @@ const app = new cdk.App();
 new AwsCdkStack(app, 'AwsCdkStack', {});
 new DynamodbStack(app, 'DynamodbStack', {});
 new LambdaStack(app, 'LambdaStack', {});
+//below line too is same only here we are specifying that the stack will run in default account and region of aws configure
+//new LambdaStack(app, 'LambdaStack', {env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION}});
+//below line too is same only here we are specifying that the stack will run in mentioned account and region
+//can be used to create stack in different region of same account
+//new LambdaStack(app, 'LambdaStack', {env: { account: '123456789012', region: 'us-east-1'}});
